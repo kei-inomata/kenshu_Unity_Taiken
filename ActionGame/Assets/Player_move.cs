414b7b9c8c577f1530c42c1ad002ad8c2c3c5001
kei-inomata <bhki1270349@gn.iwasaki.ac.jp>
2019-12-18 18:05:41 +0900

    first commit

---
+++ ActionGame/Assets/Player_move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, 5);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0, -5);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(5, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-5, 0, 5);
+        }
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0, 4, -30);
+        }
+    }
+
+        void OnCollisionStay(Collision hit)
+        {
+            if(hit.gameObject.tag == "Enemy")
+            {
+                transform.position = new Vector3(0, 4, -30);
+            }
+        }
+    
+}
